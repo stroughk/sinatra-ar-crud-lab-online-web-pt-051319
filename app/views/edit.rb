1bcff8ea51c9efa0d55e2d90e4fe67c7c48a9a54
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-17 21:19:16 +0000

    Automatically backed up by Learn

--- app/views/edit.rb
+++ app/views/edit.rb
@@ -1,4 +1,4 @@
-<form> action="/articles/<%= @article.id %>" method="post">
+<form action="/articles/<%= @article.id %>" method="post">
   <input type="hidden" name="_method" value="patch">
   <ul>
     <li>
